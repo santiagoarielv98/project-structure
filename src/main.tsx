c51f1ca68c19b2ebac216b40e6cad0bee8da3225
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-09 09:53:02 -0300

    tailwind y shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Button>Click me</Button>
+  </React.StrictMode>
+);
